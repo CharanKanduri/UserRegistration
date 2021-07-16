087ac363a4c3182aae653704b3ea479ef89cf24e
charan_kanduri <charan2000kanduri@gmail.com>
2021-07-16 22:21:49 +0530

    [CHARAN]Created User Registration

---
+++ UserRegistration/GetUserInformation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class GetUserInformation
+    {
+        public static void FirstName()
+        {   //User input
+            Console.WriteLine("\nEnter the First Name : ");
+            string firstName;
+
+            //Defining Regex for user's FirstName
+            Regex regex = new Regex(@"^[A-Z][a-z]{2,}$");
+
+            while (true)
+            {
+                firstName = Console.ReadLine();
+                if (regex.IsMatch(firstName))
+                {
+                    break;
+                }
+                else 
+                {
+                    Console.WriteLine("\nPlease Enter Valid FirstName");
+                }
+            }
+            Console.WriteLine("\nYour First Name is {0}", firstName);
+        }
+    }
+}
---
+++ UserRegistration/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration");
+            GetUserInformation.FirstName();
+        }
+    }
+}
